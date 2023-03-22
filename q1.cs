629c7fb6617b0b1855efad4ca631c111c6d2977e
vennela jangam <vennela.jangam@nagarro.com>
2023-03-22 13:48:14 +0530

    first commit

---
+++ q1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace q1
+{
+    class Equipment
+    {
+
+
+        private string name;
+        private string description;
+        private double maintenanceCost;
+
+
+        public Equipment() { }
+       
+        public Equipment(string name, string description, double maintenanceCost)
+        {
+            this.name = name;
+            this.description = description;
+            this.maintenanceCost = maintenanceCost;
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+
+        public double MaintenanceCost
+        {
+            get { return maintenanceCost; }
+            set { maintenanceCost = value; }
+        }
+
+
+    }
+
+
+    class Mobile : Equipment
+    {
+        private int distanceMoved;
+      
+        public Mobile(string name, string description, double maintenanceCost)
+            : base(name, description, maintenanceCost)
+        {
+            this.distanceMoved = 0;
+        }
+
+
+        public int DistanceMoved
+        {
+            get { return distanceMoved; }
+            set { distanceMoved = value; }
+        }
+
+
+    }
+
+
+    class Immobile : Equipment
+    {
+    
+        public Immobile(string name, string description, double maintenanceCost)
+            : base(name, description, maintenanceCost)
+        {
+
+
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Equipment> equipments = new List<Equipment>();
+
+
+            int choice = -1;
+            while (choice != 12)
+            {
+                Console.WriteLine("1. Create an equipment : mobile and immobile");
+                Console.WriteLine("2. Delete");
+                Console.WriteLine("3. Move mobile");
+                Console.WriteLine("4. List all equipment");
+                Console.WriteLine("5. Show details");
+                Console.WriteLine("6. List all mobile equipment");
+                Console.WriteLine("7. List all Immobile equipment");
+                Console.WriteLine("8. List all equipment that have not been moved till now");
+                Console.WriteLine("9. Delete all equipment");
+                Console.WriteLine("10. Delete all immobile equipment");
+                Console.WriteLine("11. Delete all mobile equipment");
+                Console.WriteLine("12. Exit");
+                Console.Write("Your choice: ");
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("\nSelect correct menu item.\n");
+                }
+                else
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            createEq(equipments);
+                            break;
+                        case 2:
+                            deleteEq(equipments);
+                            break;
+                        case 3:
+                            moveEq(equipments);
+                            break;
+                        case 4:
+                            listAllEq(equipments);
+                            break;
+                        case 5:
+                            showdetails(equipments);
+                            break;
+                        case 6:
+                            listAllMobileEq(equipments);
+                            break;
+                        case 7:
+                            listAllImmobileEq(equipments);
+                            break;
+                        case 8:
+                            listAllEquipmentNotBeenMovedTillNow(equipments);
+                            break;
+                        case 9:
+                            //Delete all equipment
+                            equipments.Clear();
+                            Console.WriteLine("\nAll equipments have been deleted.\n");
+                            break;
+                        case 10:
+                            equipments.RemoveAll(e => e is Immobile);
+                            Console.WriteLine("\nAll Immobile equipments have been deleted.\n");
+                            break;
+                        case 11:
+                            equipments.RemoveAll(e => e is Mobile);
+                            Console.WriteLine("\nAll Mobile equipments have been deleted.\n");
+                            break;
+                        case 12:
+                            //exit
+                            break;
+                        default:
+                            Console.WriteLine("\nSelect correct menu item.\n");
+                            break;
+                    }
+                }
+            }
+        }
+      
+        static void createEq(List<Equipment> equipments)
+        {
+            string name;
+            string description;
+            double maintenanceCost;
+            int choice;
+            Console.WriteLine("1. Create mobile equipment");
+            Console.WriteLine("2. Create immobile equipment");
+            Console.Write("Your choice: ");
+            if (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 2)
+            {
+                Console.WriteLine("\nSelect correct menu item.\n");
+            }
+            else
+            {
+                Console.Write("Enter the name: ");
+                name = Console.ReadLine();
+                Console.Write("Enter the description: ");
+                description = Console.ReadLine();
+                Console.Write("Enter the maintenance cost: ");
+                if (!double.TryParse(Console.ReadLine(), out maintenanceCost) || maintenanceCost < 0)
+                {
+                    Console.WriteLine("\nEnter correct the maintenance cost>0.\n");
+                }
+                if (choice == 1)
+                {
+                    equipments.Add(new Mobile(name, description, maintenanceCost));
+                }
+                if (choice == 2)
+                {
+                    equipments.Add(new Immobile(name, description, maintenanceCost));
+                }
+                Console.WriteLine("\nA new equipment has been added.\n");
+            }
+
+
+        }
+     
+        static void deleteEq(List<Equipment> equipments)
+        {
+            if (equipments.Count > 0)
+            {
+                listAllEq(equipments);
+                int selectedMobileEquipment = -1;
+                Console.Write("Select the equipment: ");
+                if (!int.TryParse(Console.ReadLine(), out selectedMobileEquipment) || selectedMobileEquipment < 0 || selectedMobileEquipment > equipments.Count)
+                {
+                    Console.WriteLine("\nSelect correct equipment.\n");
+                }
+                else
+                {
+                    equipments.RemoveAt(selectedMobileEquipment - 1);
+                    Console.WriteLine("\nThe equipment has been deleted.\n");
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("\nYou have not added equipments yet.");
+            }
+            Console.WriteLine();
+
+
+        }
+       
+        static void moveEq(List<Equipment> equipments)
+        {
+            if (equipments.Count > 0)
+            {
+                listAllEq(equipments);
+                int selectedMobileEquipment = -1;
+                Console.Write("Select the mobile equipment: ");
+                if (!int.TryParse(Console.ReadLine(), out selectedMobileEquipment) || selectedMobileEquipment < 0 || selectedMobileEquipment > equipments.Count)
+                {
+                    Console.WriteLine("\nSelect correct mobile equipment.\n");
+                }
+                else
+                {
+                    if (equipments[selectedMobileEquipment - 1] is Mobile)
+                    {
+                        int distanceMoved;
+                        Console.Write("Enter the distance to move mobile equipment: ");
+                        if (!int.TryParse(Console.ReadLine(), out distanceMoved) || distanceMoved < 0)
+                        {
+                            Console.WriteLine("\nEnter correct the distance to move>0.\n");
+                        }
+                        else
+                        {
+                            ((Mobile)equipments[selectedMobileEquipment - 1]).DistanceMoved = distanceMoved;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nSelect mobile equipment.\n");
+                    }
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("\nYou have not added equipments yet.");
+            }
+            Console.WriteLine();
+
+
+        }
+    
+        static void listAllEq(List<Equipment> equipments)
+        {
+
+
+            if (equipments.Count > 0)
+            {
+                Console.WriteLine("\n{0,-15}{1,-25}{2,-15}", "No", "Name", "Description");
+                for (int i = 0; i < equipments.Count; i++)
+                {
+                    Console.WriteLine("{0,-15}{1,-25}{2,-15}", (i + 1), equipments[i].Name, equipments[i].Description);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nYou have not added equipments yet.");
+            }
+
+
+            Console.WriteLine();
+        }
+  
+        static void showdetails(List<Equipment> equipments)
+        {
+
+
+            if (equipments.Count > 0)
+            {
+                Console.WriteLine("\n{0,-15}{1,-15}{2,-25}{3,-35}{4,-15}", "No", "Type", "Name", "Description", "Cost");
+                for (int i = 0; i < equipments.Count; i++)
+                {
+                    string type = "Immobile";
+                    if (equipments[i] is Mobile)
+                    {
+                        type = "Mobile";
+                    }
+                    Console.WriteLine("{0,-15}{1,-15}{2,-25}{3,-35}{4,-15}", (i + 1), type, equipments[i].Name, equipments[i].Description, equipments[i].MaintenanceCost);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nYou have not added equipments yet.");
+            }
+            Console.WriteLine();
+        }
+
+
+       
+        static void listAllMobileEq(List<Equipment> equipments)
+        {
+
+
+            if (equipments.Count > 0)
+            {
+                int i = 0;
+                Console.WriteLine("\n{0,-15}{1,-15}{2,-25}{3,-35}{4,-15}{5,-15}", "No", "Type", "Name", "Description", "Cost", "Distance moved");
+                foreach (Equipment equipment in equipments.FindAll(e => e is Mobile))
+                {
+                    Console.WriteLine("{0,-15}{1,-15}{2,-25}{3,-35}{4,-15}{5,-15}", (i + 1), "Mobile", equipment.Name, equipment.Description, equipment.MaintenanceCost, (((Mobile)equipment).DistanceMoved));
+                    i++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nYou have not added equipments yet.");
+            }
+            Console.WriteLine();
+        }
+
+        static void listAllImmobileEq(List<Equipment> equipments)
+        {
+
+
+            if (equipments.Count > 0)
+            {
+                int i = 0;
+                Console.WriteLine("\n{0,-15}{1,-15}{2,-25}{3,-35}{4,-15}", "No", "Type", "Name", "Description", "Cost");
+                foreach (Equipment equipment in equipments.FindAll(e => e is Immobile))
+                {
+                    Console.WriteLine("{0,-15}{1,-15}{2,-25}{3,-35}{4,-15}", (i + 1), "Immobile", equipment.Name, equipment.Description, equipment.MaintenanceCost);
+                    i++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nYou have not added equipments yet.");
+            }
+            Console.WriteLine();
+        }
+        
+        static void listAllEquipmentNotBeenMovedTillNow(List<Equipment> equipments)
+        {
+
+
+            if (equipments.Count > 0)
+            {
+                int i = 0;
+                Console.WriteLine("\n{0,-15}{1,-15}{2,-25}{3,-35}{4,-15}", "No", "Type", "Name", "Description", "Cost");
+                foreach (Equipment equipment in equipments.FindAll(e => e is Mobile && (((Mobile)e).DistanceMoved) == 0))
+                {
+                    Console.WriteLine("{0,-15}{1,-15}{2,-25}{3,-35}{4,-15}", (i + 1), "Mobile", equipment.Name, equipment.Description, equipment.MaintenanceCost);
+                    i++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nYou have not added equipments yet.");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
---
+++ q2.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Duck
+{
+    public string Name { get; set; }
+    public int Weight { get; set; }
+    public int NumWings { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Duck> ducks = new List<Duck>();
+
+        while (true)
+        {
+            Console.WriteLine("1.Add a duck ");
+            Console.WriteLine("2.Remove a duck ");
+            Console.WriteLine("3.View all ducks");
+            Console.WriteLine("4.Delete all ducks");
+            Console.WriteLine("5.Sort ducks by weight");
+            Console.WriteLine("6.Sort ducks by number of wings");
+            Console.WriteLine("7.Exit");
+
+            Console.Write("\nEnter choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    AddDuck(ducks);
+                    break;
+                case 2:
+                    RemoveDuck(ducks);
+                   
+                    break;
+                case 3:
+                    ViewAllDucks(ducks);
+                    
+                    break;
+                case 4:
+                    RemoveAllDucks(ducks);
+                    break;
+                case 5:
+                    SortDucksByWeight(ducks);
+                    break;
+                case 6:
+                    SortDucksByNumWings(ducks);
+                    break;
+                case 7:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+
+    static void ViewAllDucks(List<Duck> ducks)
+    {
+        if (ducks.Count == 0)
+        {
+            Console.WriteLine("No ducks");
+            return;
+        }
+
+        Console.WriteLine("{0,-10} {1,-10} {2,-10}", "Name", "Weight", "# Wings");
+        foreach (Duck duck in ducks)
+        {
+            Console.WriteLine("{0,-10} {1,-10} {2,-10}", duck.Name, duck.Weight, duck.NumWings);
+        }
+    }
+
+    static void AddDuck(List<Duck> ducks)
+    {
+        Duck duck = new Duck();
+
+        Console.Write("Enter duck name: ");
+        duck.Name = Console.ReadLine();
+
+        Console.Write("Enter duck weight: ");
+        duck.Weight = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter duck number of wings: ");
+        duck.NumWings = int.Parse(Console.ReadLine());
+
+        ducks.Add(duck);
+        Console.WriteLine("Duck added");
+    }
+
+    static void RemoveDuck(List<Duck> ducks)
+    {
+        Console.Write("Enter duck name to remove: ");
+        string name = Console.ReadLine();
+
+        Duck duck = ducks.Find(d => d.Name == name);
+        if (duck != null)
+        {
+            ducks.Remove(duck);
+            Console.WriteLine("Duck removed");
+        }
+        else
+        {
+            Console.WriteLine("Duck not found");
+        }
+    }
+
+    static void RemoveAllDucks(List<Duck> ducks)
+    {
+        ducks.Clear();
+        Console.WriteLine("All ducks removed");
+    }
+
+    static void SortDucksByWeight(List<Duck> ducks)
+    {
+        ducks.Sort((d1, d2) => d1.Weight.CompareTo(d2.Weight));
+        Console.WriteLine("Ducks sorted by weight");
+    }
+
+    static void SortDucksByNumWings(List<Duck> ducks)
+    {
+        ducks.Sort((d1, d2) => d1.NumWings.CompareTo(d2.NumWings));
+        Console.WriteLine("Ducks sorted by number of wings");
+    }
+}
